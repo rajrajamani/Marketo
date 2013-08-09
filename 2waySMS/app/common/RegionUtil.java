e432a8c8d9a33f90e93ba27d901bddb5a2aa9e09
Raj Rajamani <rrajamani@marketo.com>
2013-08-09 09:12:18 -0700

    Fixing Quebec

--- 2waySMS/app/common/RegionUtil.java
+++ 2waySMS/app/common/RegionUtil.java
@@ -140,7 +140,7 @@ public class RegionUtil {
 
 		statesList.put("Prince Edward Island", "PE");
 		statesList.put("Quebec", "QC");
-		statesList.put("QuÃ©bec", "QC");
+		statesList.put("Québec", "QC");
 		statesList.put("Saskatchewan", "SK");
 		statesList.put("Yukon", "YT");
 
