23f73bdfd83aad011537ad4179d770e27518d518
Raj Rajamani <rrajamani@marketo.com>
2013-08-28 09:08:36 -0700

    fix

--- 2waySMS/app/controllers/Security.java
+++ 2waySMS/app/controllers/Security.java
@@ -6,7 +6,7 @@ import models.User;
 public class Security extends Secure.Security {
 
 	static boolean authenticate(String username, String password) {
-		User user = User.find("byMunchkinId", username.toLowerCase()).first();
+		User user = User.find("byMunchkinId", username.toUpperCase().trim()).first();
 		return user != null
 				&& user.password.equals(Crypto.passwordHash(password));
 	}
