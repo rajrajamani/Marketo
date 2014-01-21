0f52ce7f923ef3662155b80d66f6cb6b6cb07f9a
Raj Rajamani <rrajamani@marketo.com>
2014-01-20 16:02:01 -0800

    getMultipleLeads by Id

--- MarketoRESTSamples/src/LeadAPI.java
+++ MarketoRESTSamples/src/LeadAPI.java
@@ -1,8 +1,8 @@
 import java.io.IOException;
-import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Request;
@@ -10,7 +10,6 @@ import org.apache.http.client.fluent.Response;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 
 public class LeadAPI {
 
@@ -25,19 +24,56 @@ public class LeadAPI {
 		if (attrMap.result.size() == 0) {
 			return null;
 		} else {
+			//return the first one
 			Map<String, String> ldAsMap = attrMap.result.iterator().next();
 			Lead ld = new Lead(ldAsMap);
 			return ld;
 		}
 	}
 
+	public static ArrayList<Lead> getMultipleLeadsById(AuthToken at, int[] ids)
+			throws ClientProtocolException, IOException {
+		String url = Constants.REST_SRVR + "/v1/leads.json?access_token=" + at.access_token + "&filterType=id&filterValues=";
+		String idStr = "";
+		for (int i : ids) {
+			idStr += i + ",";
+		}
+		url +=idStr;
+		Response response = Request.Get(url).execute();
+		Gson gson = new GsonBuilder().create();
+		String json = response.returnContent().asString();
+		LeadResponse attrMap = gson.fromJson(json, LeadResponse.class);
+		if (attrMap.result.size() == 0) {
+			return null;
+		} else {
+			Map<String, String> ldAsMap = null;
+			ArrayList<Lead> leadArr = new ArrayList<Lead>();
+			Iterator<Map<String, String>> it = attrMap.result.iterator();
+			while (it.hasNext()) {
+				ldAsMap = it.next();
+				Lead ld = new Lead(ldAsMap);
+				leadArr.add(ld);
+			}
+			return leadArr;
+		}
+	}
 	public static void main(String[] args) throws ClientProtocolException,
 			IOException {
 		AuthToken at = IdentityServer.getAuthToken(Constants.CLIENT_ID,
 				Constants.CLIENT_SECRET);
+		/*
 		Lead ld = getLeadById(at, 60);
 		ld.printLeadAttributes();
 		String lastName = ld.getLeadAttrib("lastName");
 		System.out.println(lastName);
+		*/
+		
+		int[] leadIds = {17,24};
+		ArrayList<Lead> leads = getMultipleLeadsById(at, leadIds);
+		for (Lead lead : leads) {
+			lead.printLeadAttributes();
+		}
+		
+
 	}
 }
