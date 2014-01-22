e2ac849ede47a1f3ddc15f7fcba3e2600e28e046
Raj Rajamani <rrajamani@marketo.com>
2014-01-22 15:31:15 -0800

    Removing REST Samples directory

--- MarketoRESTSamples/src/com/marketo/rest/leadapi/client/LeadResponse.java
+++
@@ -1,9 +0,0 @@
-package com.marketo.rest.leadapi.client;
-import java.util.Collection;
-import java.util.Map;
-
-public class LeadResponse {
-	public String requestId;
-	public boolean success;
-	public Collection<Map<String, String>> result;
-}
--- MarketoRESTSamples/src/com/marketo/rest/oauth/client/AuthToken.java
+++
@@ -1,8 +0,0 @@
-package com.marketo.rest.oauth.client;
-
-public class AuthToken {
-	public String access_token;
-	public String token_type;
-	public String expires_in;
-	public String scope;
-}
